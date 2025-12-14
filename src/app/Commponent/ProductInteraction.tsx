81ce6f84e75a42613facc61f46e7a4148ac76a17
Mohammedshb <hamodashbitah@gmail.com>
2025-12-15 00:49:17 +0200

    finalpart

---
+++ src/app/Commponent/ProductInteraction.tsx
@@ -0,0 +1,93 @@
+'use client'
+
+import { ProductType } from '@/types'
+import { Minus, Plus } from 'lucide-react'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import {  useState } from 'react'
+
+const ProductInteractions = ({
+  product,
+  selectedSize,
+  selectedColor,
+}: {
+  product: ProductType
+  selectedSize: string
+  selectedColor: string
+}) => {
+  const router = useRouter()
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+  const [quantity, setQuantity] = useState(1)
+
+  const handleTypeChange = (type: string, value: string) => {
+    const params = new URLSearchParams(searchParams.toString())
+    params.set(type, value)
+    router.push(`${pathname}?${params.toString()}`, { scroll: false })
+  }
+  return (
+    <div className="flex flex-col gap-4 mt-4">
+      {/* Size */}
+      <div className="flex flex-col gap-2 text-xs">
+        <span className="text-gray-500">Size</span>
+        <div className="flex items-center gap-2">
+          {product.sizes.map((size) => (
+            <div
+              className={`cursor-pointer border-1 p-[2px]  ${
+                selectedSize === size ? 'border-gray-600' : 'bordar-gray-300'
+              }`}
+              key={size}
+              onClick={() => handleTypeChange('size', size)}
+            >
+              <div
+                className={`w-6 h-6 text-center flex items-center justify-center  ${
+                  selectedSize === size
+                    ? 'bg-black text-white'
+                    : 'bg-white text-black'
+                }`}
+              >
+                {size.toUpperCase()}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* COLOR */}
+      <div className="flex flex-col gap-2 text-sm">
+        <span className="text-gray-500">Color</span>
+        <div className="flex items-center gap-2">
+          {product.colors.map((color) => (
+            <div
+              className={`cursor-pointer border-1 p-[2px]  ${
+                selectedColor === color ? 'border-gray-300' : 'bordar-white'
+              }`}
+              key={color}
+              onClick={() => handleTypeChange('color', color)}
+            >
+              <div className={`w-6 h-6 `} style={{ backgroundColor: color }} />
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* QUANTITY */}
+      <div className="flex flex-col gap-2 text-sm">
+          <span className="text-gray-500">Quantity</span>
+          <div className="flex items-center gap-2">
+            <button className="cursor-pointer border-1  border-gray-300  p-1">
+                <Minus className="w-4 h-4" />
+            </button>
+            <span>{quantity}</span>
+            <button className="cursor-pointer border-1  border-gray-300  p-1">
+                <Plus className="w-4 h-4" />
+            </button>
+          </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default ProductInteractions
+
+// Temporary
+
+//
