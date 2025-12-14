c14d8d8bb7b085edc306e73990cc63b12ec85794
Mohammedshb <hamodashbitah@gmail.com>
2025-12-14 06:26:16 +0200

    finsh zustand part now i need to design product page

--- src/app/Commponent/ShoppingCarticon.tsx
+++ src/app/Commponent/ShoppingCarticon.tsx
@@ -6,13 +6,17 @@ import useCartStore from "../stores/cartStore";
 
 const ShoppingCartIcon = () => {
 
-     const {cart} =  useCartStore();   
+     const {cart, hasHydrated} =  useCartStore();   
+
+     if (!hasHydrated) return null;
+
 
     return(
         <Link href="/cart" className="relative">
            <ShoppingCart   className="w-4 h-4 text-gray-600" />
               <span className="absolute -top-3 -right-3 
-              bg-amber-400 text-gray-600 rounded-full w-4 h-4 flex items-center justify-center text-xs font-medium">{cart.length}</span>
+              bg-amber-400 text-gray-600 rounded-full w-4 h-4 flex items-center justify-center text-xs font-medium">
+                {cart.reduce((acc,item)=>acc+item.quantity,0)}</span>
         </Link>
     )
     
--- src/types.ts
+++ src/types.ts
@@ -52,6 +52,7 @@ export type PaymentFormInputs = z.infer<typeof PaymentFormSchema>;
 
 export type CartStoreStateType = {
   cart: CartItemsType;
+  hasHydrated:boolean;
 };
 
 export type CartStoreActionsType = {
